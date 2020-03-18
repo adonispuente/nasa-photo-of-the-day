d9ecc0b8501eb48a028235a92a292ee59fb1e681
Adonis Puente <adonispuente@Adoniss-Air.attlocal.net>
2020-03-18 19:13:20 -0400

    done no stretch

---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+function Header() {
+  return (
+    <div className="logo">
+      <img
+        src={
+          "https://images.unsplash.com/photo-1541873676-a18131494184?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1261&q=80"
+        }
+      ></img>
+    </div>
+  );
+}
+export default Logo;
